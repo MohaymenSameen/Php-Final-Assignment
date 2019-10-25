3b0006f0baa243f4d5499b19d79ce2e8d50decf5
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2019-10-25 19:16:12 +0200

    edited the edit user page
    user can edit his/her details after logging and a confirmation email is sent

--- edit_user.php
+++ edit_user.php
@@ -1,10 +1,3 @@
-<?php
-
-include_once 'Db_Connection/db.connection.php';
-$sql="SELECT * FROM register";
-$result=mysqli_query($conn,$sql);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,15 +27,79 @@ $result=mysqli_query($conn,$sql);
     
     <div class="background_color">
         <form class="edit_details_form" method="POST" ation="#">
-            <h1>Edit Details</h1><br><br>           
-            <input type="text" name="firstname" placeholder="First Name"><br><br> 
-            <input type="text" name="lastname" placeholder="Last Name"><br><br> 
-            <input type="text" name="email_address" placeholder="Email"><br><br>                      
-            <input type="password" name="password" placeholder="Password"><br><br>
-            <input type="password" name="confirm_password" placeholder="Confirm Password"><br><br>            
-            <input type="submit" name="change" value="Change Details"><br><br><br>   
+        <h1>Edit Details</h1><br><br>  
+            <?php
+                
+                include_once 'Db_Connection/db.connection.php';
+
+                session_start();
+                $sql="SELECT * FROM register WHERE email_address='{$_SESSION['username']}'";
+                $result=mysqli_query($conn,$sql);   
+                while($row=mysqli_fetch_assoc($result))
+                {
+                    echo '<input type="text" name="firstname" placeholder="First Name" value='.$row['firstname'].'><br><br>';
+                    echo '<input type="text" name="lastname" placeholder="Last Name" value='.$row['lastname'].'><br><br>';
+                    echo '<input type="text" name="email_address" placeholder="Email Address" value='.$row['email_address'].'><br><br>';
+                    echo '<h4>Change Password?</h4>';
+                    echo '<input type="password" name="password" placeholder="Password"><br><br>';
+                    echo '<input type="password" name="confpassword" placeholder="Confirm Password"><br><br>';
+                }
+                if(isset($_POST['change']))
+                {
+                    
+                    $firstname=mysqli_real_escape_string($conn,$_POST['firstname']);
+                    $lastname=mysqli_real_escape_string($conn,$_POST['lastname']);
+                    $email_address=mysqli_real_escape_string($conn,$_POST['email_address']);
+                    $password=mysqli_real_escape_string($conn,$_POST['password']); 
+                    $confpassword=mysqli_real_escape_string($conn,$_POST['confpassword']);
+                    $hash = password_hash($password, PASSWORD_BCRYPT); 
+
+                    if(empty($password) && empty($confpassword))
+                    {
+                        $sql1="UPDATE `register` SET firstname='$firstname',lastname='$lastname',email_address='$email_address' WHERE email_address='{$_SESSION['username']}'";
+                        $result1=mysqli_query($conn,$sql1);
+
+                        if($result1)
+                        {
+                            exit ("Database has been upadated");
+                            mail($email,"Account details updated","Your details have been changed recently by you","From: randomemail@domain.com\r\n");
+
+                        }
+                        else
+                        {
+                            exit ("Error has happened");
+                        }
+                        
+                    }
+                    else if($password!=$confpassword)
+                    {
+                        exit ("Passwords dont match!");
+                    }
+                    else
+                    {
+                        $sql1="UPDATE `register` SET firstname='$firstname',lastname='$lastname',email_address='$email_address',password='$hash' WHERE email_address='{$_SESSION['username']}'";
+                        $result1=mysqli_query($conn,$sql1);
+                        
+                        if($result1)
+                        {
+                            exit ("Database has been upadated");
+                            mail($email,"Account details updated","Your details and password have been changed recently by you","From: randomemail@domain.com\r\n");
+
+                        }
+                        else
+                        {
+                            exit ("Error has happened");
+                        }
+                    }
+                    
+
+                   
+                }
+            ?>                  
+            <input type="submit" name="change" value="Save Changes"><br><br><br>   
         </form>
     </div> 
 
 </body>
 </html>
+
