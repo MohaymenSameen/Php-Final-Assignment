23555b2ed7d83c67a97cd80e6b78ece7e9d9092a
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2019-11-08 22:09:37 +0100

    some stuff changed
    some stuff have been changed some it works properly

--- controllers/forgotpass_controller.php
+++ controllers/forgotpass_controller.php
@@ -10,7 +10,7 @@
 
                 if($ForgotPassModel->getEmail($email_address))
                 {
-                    $url="http://627650.infhaarlem.nl/reset-password.php?email_address=$email_address";
+                    $url="http://627650.infhaarlem.nl/views/resetpass_view.php?email_address=$email_address";
                     mail($email_address,"Reset Password","To reset your password, please click the link: $url","From: randomemail@domain.com\r\n");
                     exit ("Password reset link has been sent to your email");
                 }
--- reset-password.php
+++ reset-password.php
@@ -63,12 +63,12 @@
     
         <div class="navigation_bar">
             
-            <a href="home.php">Home</a>
+            <a href="/views/home_view.php">Home</a>
             <a href="#Laptops">Laptops</a>
             <a href="#Phones">Phones</a>
             <a href="#Cameras">Cameras</a>
             <a href="#Tvs">Tvs</a>
-            <a href="Login.php"><strong>Join/Sign In</strong></a>
+            <a href="/views/login_view.php"><strong>Join/Sign In</strong></a>
         </div>
     </header>    
     
